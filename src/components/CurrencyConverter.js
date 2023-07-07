605bb2cd43487ec9e33778413417595b69cc1a1c
Ashraf Makki <makkiashraf08@gmail.com>
2023-07-06 21:45:43 -0400

    Finishd currency converter

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,23 +1,52 @@
+import { useEffect, useState } from "react";
+
 function CurrencyConverter() {
-  // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [targetCurrency, setTargetCurrency] = useState("");
+  const [convertedAmt, setConvertedAmt] = useState("");
+
+  useEffect(
+    function () {
+      async function convert() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=${targetCurrency}`
+        );
+
+        const data = await res.json();
+        setConvertedAmt(data.rates[targetCurrency]);
+      }
+
+      if (!amount || targetCurrency === currency) return;
+      convert();
+    },
+    [amount, currency, targetCurrency]
+  );
 
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
 
-      <select>
-        <option value="USD">USD</option>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
+      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>Output</p>
+      <select
+        value={targetCurrency}
+        onChange={(e) => setTargetCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{targetCurrency === currency ? amount : convertedAmt}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import App from "./App";
 import StarRating, { Test } from "./components/StarRating";
 import CurrencyConverter from "./components/CurrencyConverter";
@@ -8,8 +8,8 @@ import CurrencyConverter from "./components/CurrencyConverter";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <CurrencyConverter />
+    <App />
+    {/* <CurrencyConverter /> */}
     {/* <StarRating maxRating={10} /> */}
     {/* <StarRating size={22} /> */}
     {/* <Test /> */}
